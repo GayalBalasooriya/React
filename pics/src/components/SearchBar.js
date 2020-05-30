74990fcb4583569da7109f951effa4a6b868c5a7
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-30 21:35:39 +0530

    added until handling form submitttal

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = {
+        term: ''
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
+
+    // render() {
+    //     return(
+    //         <div className="ui segment">
+    //             <form className="ui form">
+    //                 <div className="field">
+    //                     <label>Image Search</label>
+    //                     <input type="text" onChange={this.onInputChange}/>
+    //                 </div>
+    //             </form>
+    //         </div>
+    //     )    
+    // };
+
     render() {
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" 
+                            value={this.state.term} 
+                            onChange={e => this.setState({
+                                term: e.target.value
+                            })}
+                        />
                     </div>
                 </form>
             </div>
         )    
     };
+
+
 }
 
 export default SearchBar;
