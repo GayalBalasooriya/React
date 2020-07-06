05acdbd3303d07d6f40070edc65b71404dfd206f
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-06 20:09:44 +0530

    Added until how to fetch data in a redux app

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class PostList extends React.Component {
+    render() {
+        return (
+            <div>PostList</div>
+        )
+    }
+}
+
+export default PostList
