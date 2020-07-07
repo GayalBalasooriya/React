42d92d9e8fcd1ede254039d994fb11eb6737f7e9
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-07 23:53:59 +0530

    End of the async actions with redux thunk

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
     render() {
         return (
             <div>PostList</div>
@@ -8,4 +15,4 @@ class PostList extends React.Component {
     }
 }
 
-export default PostList
+export default connect(null, { fetchPosts })(PostList)
