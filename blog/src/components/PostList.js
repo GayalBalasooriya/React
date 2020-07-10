e43a9952325b6f6e5ccea1e996b4be1313ef81bb
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-10 10:34:08 +0530

    Added until list building

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,10 +8,26 @@ class PostList extends React.Component {
         this.props.fetchPosts()
     }
 
+    renderList() {
+        return this.props.posts.map(post => {
+            return(
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
-        console.log(this.props.posts)
+        //console.log(this.props.posts)
         return (
-            <div>PostList</div>
+            <div className="ui relaxed divided list">{this.renderList()}</div>
         )
     }
 }
