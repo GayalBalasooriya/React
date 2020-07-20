a6436552fdc41d5547e6638309a1c109fc632750
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-20 10:08:21 +0530

    added until memoization issues

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from "../apis/jsonPlaceholder"
 
 export const fetchPosts = () => async (dispatch) => {
@@ -9,11 +10,22 @@ export const fetchPosts = () => async (dispatch) => {
     })
 }    
 
-export const fetchUser = (id) => async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`)
+// export const fetchUser = (id) => async (dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`)
 
-    dispatch({
-        type: 'FETCH_USER',
-        payload: response.data
-    })
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// }
+
+export const fetchUser = function(id) {
+    return async function(dispatch) {
+        const response = await jsonPlaceholder.get(`/users/${id}`)
+
+        dispatch({
+            type: 'FETCH_USER',
+            payload: response.data
+        })
+    }
 }
