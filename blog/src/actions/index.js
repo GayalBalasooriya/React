c1a429db89b78533ac65f391e32f141884c0fa62
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-26 13:47:05 +0530

    added until one time memoization

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -19,13 +19,15 @@ export const fetchPosts = () => async (dispatch) => {
 //     })
 // }
 
-export const fetchUser = function(id) {
-    return async function(dispatch) {
-        const response = await jsonPlaceholder.get(`/users/${id}`)
-
-        dispatch({
-            type: 'FETCH_USER',
-            payload: response.data
-        })
-    }
+export const fetchUser = id => dispatch => {
+    _fetchUser(id, dispatch)
 }
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    })
+})
