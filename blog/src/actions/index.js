8c621cb63e1706381fdfd3c725d7851412c613a1
Gayal Balasooriya <baigayal01@gmail.com>
2020-07-27 22:14:12 +0530

    added until alternate overfetching solution

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -19,15 +19,29 @@ export const fetchPosts = () => async (dispatch) => {
 //     })
 // }
 
-export const fetchUser = id => dispatch => {
-    _fetchUser(id, dispatch)
-}
+// Memoize Version
+// export const fetchUser = id => dispatch => {
+//     _fetchUser(id, dispatch)
+// }
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`)
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
+
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`)
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     })
-})
+}
+
+
+   
+
