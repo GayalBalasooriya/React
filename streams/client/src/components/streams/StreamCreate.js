1afd6b8e1edf96a7e3cce6f0220e4f5e0cfaf294
Gayal Balasooriya <baigayal01@gmail.com>
2020-08-31 17:44:35 +0530

    added until automatically handling events

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+
+    renderInput(formProps) {
+        return <input { ...formProps.input } />
+    }
+
+    render() {
+        return <form>
+            <Field name="title" component={this.renderInput} />
+            <Field name="description" component={this.renderInput} />
+        </form>
+    }
 }
-
-export default StreamCreate;
+    
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
